2be10ecd60c58464156dc78cc9a756534baeeab7
Kamalini Sahoo <sahookamalini34@gmail.com>
2024-07-03 14:15:08 +0530

    Updates

---
+++ precache-manifest.b046554364a366d6d355e021907351cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec816920bd04b157172d4b721918fd09",
+    "url": "/pencilpal/index.html"
+  },
+  {
+    "revision": "24583d970b0ac69a0543",
+    "url": "/pencilpal/static/css/2.dd6d8973.chunk.css"
+  },
+  {
+    "revision": "81e80b2b73db2d12b861",
+    "url": "/pencilpal/static/css/main.3d4cc6ab.chunk.css"
+  },
+  {
+    "revision": "24583d970b0ac69a0543",
+    "url": "/pencilpal/static/js/2.14ad0114.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pencilpal/static/js/2.14ad0114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e80b2b73db2d12b861",
+    "url": "/pencilpal/static/js/main.8df2153f.chunk.js"
+  },
+  {
+    "revision": "eb7bcd0497bf4984121a",
+    "url": "/pencilpal/static/js/runtime-main.29bfa696.js"
+  }
+]);
